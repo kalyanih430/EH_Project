ecb821ec97034d31803a40e3b053fd22af8b07ac
kalyanih430 <68651838+kalyanih430@users.noreply.github.com>
2020-12-25 15:45:39 +0530

    Update welcome.php
    changed style

--- html/welcome.php
+++ html/welcome.php
@@ -18,8 +18,8 @@ echo "Login Successfull !! Welcome ".$_SESSION['Username'];
 
 	<style>
 		body {
-			
- 			background-image: url("https://png.pngtree.com/thumb_back/fw800/background/20190223/ourmid/pngtree-watercolor-color-city-background-design-backgroundcity-backgroundbackground-materialcivilizationgreen-image_70171.jpg	");
+			text-align:center;
+ 			background-image: url("https://i.pinimg.com/originals/0d/af/b7/0dafb7928a99a9cb552a941895a6e586.jpg");
   			height: 100%;
 			background-position: center;
  		        background-repeat: no-repeat;
@@ -28,33 +28,47 @@ echo "Login Successfull !! Welcome ".$_SESSION['Username'];
 		
 		
 		.comment-box {
-	    		width: 320px;
-  			padding: 10px;
+			text-align:left;
+	    		width: 520px;
+  			padding: 5px;
   			border: 1px solid gray;
   			margin: 0;
 			}
 		.topnav-right {
   			float: right;
 			}
+		form {
+    			display: inline-block;
+			}
 	</style>
 
-
 <div class="topnav-right">
-    <a href='profile.php'><br><b>Profile<b></a> <br>
-   <a href='search.php'><br><b>Search<b></a> <br> <a href='logout.php'><br><b>Logout<b></a>
+
+<form method='POST' action='search.php'>
+<input type='text' name='searchvalue' placeholder='Search user'><br>
+<button type = 'submit'name='Search'>Search</button><br><br>
 </div>
+</form>
+<div>
+   <a href='profile.php'><br><b>View Profile<b></a>
+   <a href='logout.php'><br><b>Logout<b></a>
+</div>
+
+
+
 
 	  <form method='POST' action='functions.php'>
 		<h4>Post a comment:</h4>
 	  
-	    <input name="Name" type="text" placeholder="Your Name" required><br>
-	    <textarea name='Comment' id='Comment'  cols="50" rows="2" placeholder="Enter a comment"></textarea><br><br>
- 	     <button type = 'submit' name='CommentSubmit'>Comment</button>
+	    <input name="Name" type="text" placeholder="Your Name" required>
+	    <textarea name='Comment' id='Comment'  cols="50" rows="2" placeholder="Enter a comment"></textarea>
+ 	    <button type = 'submit' name='CommentSubmit'>Comment</button>
 		
-	 </form>
+	 
 
 
 	<?php
+	
 
 
 	$conn = mysqli_connect('localhost','admin','adpassword','EH_Project');
@@ -65,7 +79,8 @@ echo "Login Successfull !! Welcome ".$_SESSION['Username'];
 	
 	$sql = "SELECT name,comment from comments";
 	$result= $conn-> query($sql);
-	echo "<h2>Comments</h2>";
+	
+	echo "<br>Comments<br>";
 	while($row = $result->fetch_assoc()) {
 		echo "<div class='comment-box'>";
 		echo $row['name']."<br>";
@@ -76,9 +91,12 @@ echo "Login Successfull !! Welcome ".$_SESSION['Username'];
 
 	?>	
 
+          </form>
     </body>
 
 </html>
+
+
 
 
 
