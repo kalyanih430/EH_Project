576d414894e3dd96056cd2f987eef3c4d1d03cf4
kalyanih430 <68651838+kalyanih430@users.noreply.github.com>
2021-01-17 19:10:35 +0530

    changes made
    checked if the email id of the user. If it is of the attacker the user will not be able to update the details

--- html/csrf.php
+++ html/csrf.php
@@ -1,4 +1,18 @@
+<html>
+<head>
 
+	<style>
+		body {
+ 			background-image: url("https://i.pinimg.com/originals/0d/af/b7/0dafb7928a99a9cb552a941895a6e586.jpg");
+  			height: 100%;
+ 		        background-repeat: no-repeat;
+  			background-size: cover;
+			}
+		
+	</style>
+
+</head>
+</html>
 <?php
 
 session_start();
@@ -9,12 +23,21 @@ if($con == false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$sql = "update users set Name = 'Attacker', email_address='attacker@gmail.com', password ='Attacker123' where Name = '".$_SESSION['Username']."'";
+$sql1 = "SELECT * FROM users WHERE Name = '".$_SESSION['Username']."'";
+	$rs = mysqli_query($con, $sql1);
+	echo "<h2>Profile Information</h2>";
+	while($row = mysqli_fetch_array($rs)) {
+			echo "<b>Username :</b>". $row['Name'] . "<br />";
+    			echo "<b>Email Address :</b>". $row['email_address'];
+		}
+
+$sql = "update users set Name = '".$_SESSION['Username']."', email_address='attacker@gmail.com', password ='Attacker123' where Name = '".$_SESSION['Username']."'";
 $run = mysqli_query($con, $sql);
 
 if(! $run ) {
       die('Could not update data: ' . mysqli_error());
    }
-   echo "Updated data successfully\n";
-   mysqli_close($con);
+
+	echo "<br><br><a href ='update.php'> Update details </a>";
+   	mysqli_close($con);
 ?>
